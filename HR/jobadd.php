06056a85b4a9b491e6389a2e4d96d62dbabf1fcf
T-Rone <48648310+T-Rone@users.noreply.github.com>
2019-12-02 19:00:01 +0800

    Update jobadd.php

--- HR/jobadd.php
+++ HR/jobadd.php
@@ -1,9 +1,110 @@
 <html>
 <head>
+    <meta charset="utf-8" />
     <title>部门信息管理</title>
+     <style type="text/css">
+		*{
+			margin: 0;padding: 0;
+		 }
+		.header
+		{
+			width: 100%;
+			height: 425px;
+			background-image: url(img/1.jpg);
+		}
+		.menu{
+			background-color:  burlywood;
+			margin-top: -5px;
+		}
+		
+		ul{
+			margin: 0 auto;
+			list-style: none;
+			height: 30px;
+			line-height: 30px;
+		}
+		ul li
+		{
+			
+			width: 200px;
+			text-align: center;
+			margin: 0 auto;
+			margin-left: 225px;
+			float: left;
+		}
+	 	a
+		{
+			text-decoration: none;
+			margin: 0 auto;
+			padding: 6px;
+		}
+		a:hover{color: #000000;background-color: #DEE1FF;}
+		
+		.bottom p{
+			display:  block;
+			text-align: center;
+			margin: 0 auto;
+		}
+		.content
+		       {	
+				width:100%;
+		        margin:0 auto;
+		       }
+		       .content_1
+		       {
+		
+		           width:100%;border:1px solid #ccc;
+		           height:30px;
+		       }
+		       span
+		       {
+		        color: #666;
+		        font-size:13px;
+		        line-height:30px;
+		
+		       }
+		       span a
+		       {
+		        color: #666;
+		        text-decoration:none;
+		       }
+			   .list_1
+		       {
+		          height:30px;
+		          font-size:13px;
+		          color:#666;
+		          text-align:center;
+		          border:1px solid #ccc;
+		       }
+
+	</style>
 </head>
-  <meta charset="utf-8" />
 <body>
+	<div class="box">	
+		<div class="header">
+				
+		</div>
+		
+		<div class="menu">
+			 	<ul>
+			 		<li><a href="emp.php" target="ms">员工信息</a></li>
+			 		<li><a href="dept.php" target="ms">部门信息</a></li>
+					<li><a href="job.php" target="ms">职位信息</a></li>
+			 	</ul>
+			 		
+		</div>
+		
+		<div class="content">	
+		      <div class="content_1">
+		      
+		                 <form action="empaction.php?action=sel" method="post" accept-charset="utf-8">
+		                 <span>你现在所在的位置：<a href="#">首页 </a>》员工||
+		                   请输入关键字：<input type="text" name="name"/>
+		                   <input type="submit" value="查询">
+		                   </span>
+		                 </form>
+		      
+		             </div>
 <center>
     <h3>增加职位信息</h3>
     <form id="addjob" name="addjob" method="post" action="jobaction.php?action=add">
@@ -29,5 +130,6 @@
 
     </form>
 </center>
+</div>
 </body>
 </html>
